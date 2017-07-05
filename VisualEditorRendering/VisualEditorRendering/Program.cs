d4a988104b443f703272aff3db5bb7f7fcbc72ac
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-05 19:22:57 -0400

    added new stuff

---
+++ VisualEditorRendering/VisualEditorRendering/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenTK;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Graphics;
+using System.IO;
+using System.Drawing;
+
+namespace VisualEditor.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading up OpenGL. Please wait...", 0, DisplayDevice.Default);
+            Console.WriteLine("OpenGL 2.0.0 has been loaded!", 0, DisplayDevice.Default);
+            {
+                GameWindow window = new GameWindow(800, 600);
+                Game game = new Game(window);
+                window.Title = String.Format("Visual Editor BETA V0.0.0.1, OpenGL 2.0 Version!");
+                window.Run();
+                //test this.
+            }
+        }
+    }
+}
---
+++ VisualEditorRendering/VisualEditorRendering/game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using OpenTK;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace VisualEditor.cs
+{
+    class Game
+    {
+        public GameWindow window;
+
+        public Game(GameWindow windowInput)
+        {
+            this.window = windowInput;
+
+            window.Load += window_Load;
+            window.RenderFrame += Window_RenderFrame;
+            window.UpdateFrame += Window_UpdateFrame;
+            window.Closing += Window_Closing;
+        }//no.
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        void window_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(Color.FromArgb(5, 5, 25));
+        }
+
+        private void Window_UpdateFrame(object sender, FrameEventArgs e)
+        {
+
+        }
+
+        private void Window_RenderFrame(object sender, FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Flush();
+            window.SwapBuffers();
+        }
+    }
+}
