55883b5cf5cac8994e085faf1e631dac9a1910a9
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-06 02:31:02 -0400

    added and fixed some shit for texture assets.

---
+++ VisualEditorRendering/VisualEditorRendering/Assets/assetspipes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace VisualEditorRendering.Assets
+{
+    class assetspipes
+    {
+        public static int LoadTexture(string path)
+        {
+            
+        }
+    }
+}
